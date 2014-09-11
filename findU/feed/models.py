b695e1e7a47842f2786fe554631e002117f379f0
xiaoping jin <jinxp@lenovo.com>
2014-09-11 15:23:52 +0800

    add feed models for position info

--- findU/feed/models.py
+++ findU/feed/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class PosInfo(models.Model):
+	user = models.ForeignKey(User)
+	date = models.DateTimeField(auto_now_add=True)
+	lat = models.FloatField(default=0,db_index=True)
+	lng = models.FloatField(default=0,db_index=True)
+	desc = models.CharField(max_length=140)
