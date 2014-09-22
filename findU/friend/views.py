8d81ff1d0b9b6bf93d724eee41066bbd95aa3632
xiaoping jin <jinxp@lenovo.com>
2014-09-22 16:12:49 +0800

    use imsi for push tag

--- findU/friend/views.py
+++ findU/friend/views.py
@@ -32,11 +32,13 @@ def add_friend(request):
 
 		try:
 			check_user = User.objects.get(username=target_user)
+			user_info = UserInfo.objects.get(user=check_user)
+			push_target = user_info.imsi
 
 			_jpush = jpush.JPush(app_key, master_secret)
 			push = _jpush.create_push()
 			push.audience = jpush.audience(
-				jpush.tag("tag1", target_user)
+				jpush.tag("tag1", push_target)
 			)
 			push.message = jpush.message(msg_content=201, extras=src_user)
 			push.platform = jpush.all_
@@ -84,16 +86,18 @@ def ok_friend(request):
 			data['status']=34
 			data['error']='user do not exist'
 			return HttpResponse(json.dumps(data,ensure_ascii=False),content_type='application/json')
-			
+
 		target_user=request.POST.get('target_user')
 
 		try:
 			target = User.objects.get(username=target_user)
-
+			user_info = UserInfo.objects.get(user=check_user)
+			push_target = user_info.imsi
+			
 			_jpush = jpush.JPush(app_key, master_secret)
 			push = _jpush.create_push()
 			push.audience = jpush.audience(
-				jpush.tag("tag1", target_user)
+				jpush.tag("tag1", push_target)
 			)
 			push.message = jpush.message(msg_content=202, extras=src_user)
 			push.platform = jpush.all_
