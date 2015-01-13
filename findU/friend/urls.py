fb93fd958d38633fc42718330fb36e06593d19ad
heping wang <wanghp1@lenovo.com>
2015-01-13 17:04:59 +0800

    update

--- findU/friend/urls.py
+++ findU/friend/urls.py
@@ -6,4 +6,5 @@ urlpatterns=patterns('',
 	url(r'^get_friend/$',views.get_friend,name='get_friend'),
 	url(r'^accept_friend/$',views.accept_friend,name='accept_friend'),
 	url(r'^update_friend/$',views.update_friend,name='update_friend'),
+	url(r'^search_friend/$',views.search_friend,name='search_friend'),
 	)
