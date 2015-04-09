3fa22c11054e32f78faf67bc4a5f705af96f7dcc
xiaoping jin <jinxp@lenovo.com>
2015-04-09 10:21:26 +0800

    fix bug

--- findU/tips/models.py
+++ findU/tips/models.py
@@ -7,7 +7,7 @@ class Tip(models.Model):
 	message = models.CharField(max_length=280)
 	create_time = models.CharField(max_length=100)
 	photo = models.CharField(max_length=200)
-	audoo = models.CharField(max_length=200)
+	audio = models.CharField(max_length=200)
 
 	def __str__(self):
 		return self.message
--- findU/tips/tests.py
+++ findU/tips/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from user.models import UserInfo
+from tips.models import Tip
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class tipTests(TestCase):
+
+	def setUp(self):
+		user_1 = User.objects.create(username='12345993')
+		user_info_1 = UserInfo.objects.create(user=user_1, nickname='test 01')
+		user_2 = User.objects.create(username='13636630387')
+		user_info_2 = UserInfo.objects.create(user=user_2, nickname='test 02')
+
+		tip = Tip.objects.create(user=user_2,receiver="12345993",message="test4test",create_time="2015-4-9",audio="",photo="")
+
+	def test_get_tip(self):
+		json_data = {
+			"mobile": 12345993,
+			"friend_mobile": "13636630387",
+			"mesg_id": 1,
+		}
+	
+		response = self.client.post(reverse('tips:get_tip'), json_data)
+
+		self.assertEqual(response.content, "ok")	
