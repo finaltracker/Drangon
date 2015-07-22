ba710ee3fc52d60bc3e26e47f10a14c326fc4cf3
xiaoping jin <jinxp@lenovo.com>
2015-07-22 10:57:57 +0800

    merge locate_get_all and locate_update

--- findU/feed/urls.py
+++ findU/feed/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import patterns,url
 from feed import views
 
 urlpatterns=patterns('',
-	url(r'^locate_update/$',views.locate_update,name='locate_update'),
-	url(r'^locate_get_all/$',views.locate_get_all,name='locate_get_all'),
+	url(r'^locate_get/$',views.locate_get_all,name='locate_get'),
 	url(r'^locate_upload/$',views.locate_upload,name='locate_upload'),
 	)
--- findU/feed/views.py
+++ findU/feed/views.py
@@ -13,57 +13,60 @@ from utils.pack_json import toJSON
 # delta set as : half hour
 threshold = 0.5
 
-def locate_update(request):
+def locate_get(request):
 	data = {}
 
 	if request.method=='POST':		
 		logger.debug(str(request.POST))
 
-		src_user=request.POST.get('src_user')
-		target_user=request.POST.get('target_user')
+		src_user=request.POST.get('mobile')
+		target_user=request.POST.get('friend_mobile')
+		require_type=request.POST.get('requireType')
 
-		user=User.objects.get(username=target_user)		
-		target_pos=PosInfo(user=user)
-		delta = (timezone.now() - target_pos.date).hours
-		if delta < threshold:
+		if require_type == "all":
+			my_user=User.objects.get(username=src_user)
+			friends = Friend.objects.filter(user=my_user)
+			all_friends = []
+			for relative_friend in friends:
+				target_pos=PosInfo(user=relative_friend.friend)
+				relative = {}
+				relative['friend_mobile'] = relative_friend.friend.username
+				relative['lat'] = target_pos.lat
+				relative['lng'] = target_pos.lng
+				all_friends.append(relative)
+
 			data['status']=0
 			data['moible'] = src_user
-			data['friend_mobile'] = target_user
-			data['lat'] = target_pos.lat
-			data['lng'] = target_pos.lng
+			data['geo'] = all_freinds
 			return HttpResponse(toJSON(data),content_type='application/json')
+
+		else if require_type == "one":
+			user=User.objects.get(username=target_user)		
+			target_pos=PosInfo(user=user)
+			delta = (timezone.now() - target_pos.date).hours
+			if delta < threshold:
+				data['status']=0
+				data['moible'] = src_user
+
+				all_friends = []
+
+				relative = {}
+				relative['friend_mobile'] = target_user
+				relative['lat'] = target_pos.lat
+				relative['lng'] = target_pos.lng	
+				all_friends.append(relative)
+
+				data['geo'] = all_freinds
+				return HttpResponse(toJSON(data),content_type='application/json')
+			else:
+				jpush_send_message(src_user,target_user,303)
+				data['status']=401
+				return HttpResponse(toJSON(data),content_type='application/json')
 		else:
-			jpush_send_message(src_user,target_user,303)
-			data['status']=401
-			return HttpResponse(toJSON(data),content_type='application/json')
+			data['status']=19
+			return HttpResponse(toJSON(data),content_type='application/json')		
 
-	data['status']=503
-	return HttpResponse(toJSON(data),content_type='application/json')
 
-def locate_get_all(request):
-	data = {}
-
-	if request.method=='POST':		
-		logger.debug(str(request.POST))
-
-		src_user=request.POST.get('src_user')
-		target_user=request.POST.get('target_user')
-
-		my_user=User.objects.get(username=target_user)
-		friends = Friend.objects.filter(user=my_user)
-		all_friends = []
-		for relative_friend in friends:
-			target_pos=PosInfo(user=relative_friend.friend)
-			relative = {}
-			relative['friend_mobile'] = relative_friend.friend.username
-			relative['lat'] = target_pos.lat
-			relative['lng'] = target_pos.lng
-			all_friends.append(relative)
-
-		data['status']=0
-		data['moible'] = src_user
-		data['geo'] = all_freinds
-		return HttpResponse(toJSON(data),content_type='application/json')
 
 	data['status']=503
 	return HttpResponse(toJSON(data),content_type='application/json')
@@ -75,8 +78,8 @@ def locate_upload(request):
 		logger.debug(str(request.POST))
 
 		user_name=request.POST.get('mobile')
-		lant=request.POST.get('lant')
-		longt=request.POST.get('longt')
+		lant=request.POST.get('lat')
+		longt=request.POST.get('lng')
 
 		user=User.objects.get(username=user_name)
 		
