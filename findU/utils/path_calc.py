9cccccfefcf128df07bff374f6cce3aece548846
xiaoping jin <jinxp@lenovo.com>
2015-09-14 15:43:02 +0800

    calc ball track

--- findU/utils/path_calc.py
+++ findU/utils/path_calc.py
@@ -1,6 +1,9 @@
-map_table = [[3,3,8,8],[8,3,14,8],[3,9,8,12],[8,9,14,12]]
+import time
 
-position_guard = (10,5)
+china_lng = (73.33, 135.05)
+china_lat = (3.51, 53.33)
+
+
 
 class Node:
 	def __init__(self, data):
@@ -54,60 +57,29 @@ class Tree:
 
 
 if __name__ == '__main__':
+	begin = (10,5)
+	end = (20,13)
+	duration = 20
+
+	x = float(begin[0])
+	y = float(begin[1])
 	'''
-	define Node
+	linear equation
 	'''
-	a = Node('A')
-	b = Node('B')
-	c = Node('C')
-	d = Node('D')
-	e = Node('E')
-	f = Node('F')
-	g = Node('G')
-	h = Node('H')
-	i = Node('I')
-	j = Node('J')
-	k = Node('K')
-	l = Node('L')
-	m = Node('M')
-	n = Node('N')
-	o = Node('O')
-	 
-	'''
-	adding Node to build true
-	'''
-	a.add(b)
-	a.add(g)
-	a.add(h)
-	b.add(c)
-	b.add(e)
-	g.add(i)
-	g.add(j)
-	g.add(k)
-	g.add(l)
-	h.add(m)
-	h.add(n)
-	h.add(o)
-	c.add(d)
-	c.add(f)
-	i.add(Node(29))
-	j.add(Node(28))
-	k.add(Node(27))
-	l.add(Node(26))
-	m.add(Node(25))
-	n.add(Node(24))
-	o.add(Node(23))
-	f.add(Node(30))
-	 
-	 
-	tree = Tree()
-	tree.link_to_head(a)
-	 
-	 
-	#testcase
-	print 'Node',tree.search("ABE").get_data()
-	print 'Node',tree.search("ABC").get_data()
-	print 'Node',tree.search("AHM").get_data()
-	tree.insert("ABCD", 1)
-	for i in d.get_children():
-	    print 'value after', d.get_data(),' is ', i.get_data()
+	x1 = float(begin[0])
+	y1 = float(begin[1])
+	x2 = float(end[0])
+	y2 = float(end[1])
+	a = (y2 - y1)/(x2-x1)
+	b = (y2*x1-y1*x2)/(x1-x2)
+
+	step = (x2-x1)/20
+
+	print 'a: %f, b: %d, step: %f' %(a,b,step)
+	i = 1
+	while i <= duration:
+		print 'x : %d, y: %d' %(x, y)
+		x += step
+		y = a*x+b
+		i += 1
+		time.sleep(1)
