57054bdb0c904f2e9b4ef1581fd8ea8a30c69be0
xiaoping jin <jinxp@lenovo.com>
2015-11-26 10:43:54 +0800

    Change sqlite3 to postgresql

--- findU/findU/settings.py
+++ findU/findU/settings.py
@@ -66,10 +66,22 @@ WSGI_APPLICATION = 'findU.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'develop': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    },
+
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb',
+        'USER': 'www-data',
+        'PASSWORD': 'www-data',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        'TEST': {
+            'NAME': 'testdb',
+        },        
+    }    
 }
 
 BROKER_URL = 'redis://localhost:6379/0'
