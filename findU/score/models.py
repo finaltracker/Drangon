bdc451feb28e6d4c2e6b8b69b009a6e591213272
xiaoping jin <jinxp@lenovo.com>
2015-12-07 17:39:41 +0800

    REMOVE score app

--- findU/score/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class Score(models.Model):
-	user = models.ForeignKey(User)
-	score = models.IntegerField(default=0)
--- findU/user/models.py
+++ findU/user/models.py
@@ -12,6 +12,7 @@ class UserInfo(models.Model):
 	imsi=models.CharField(max_length=32,blank=True)
 	avatar=models.ImageField(upload_to='avatar')
 	version_count = models.IntegerField(default=0)
+	score = models.IntegerField(default=1000)
 	reserved=models.CharField(max_length=140)
 
 	def avatar_url(self):
