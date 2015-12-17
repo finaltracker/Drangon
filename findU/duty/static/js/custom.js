952e9368d0613f9072edb0d32999acaae5996570
xiaoping jin <jinxp@lenovo.com>
2015-12-17 14:14:30 +0800

    ADD js

--- findU/duty/static/js/custom.js
+++ findU/duty/static/js/custom.js
@@ -1,2 +1,6 @@
 function calculator() {
+  var a = document.getElementById("num_one").value;
+  var b = document.getElementById("num_two").value;
+  var res = androidInterfaceMethod.calculator(a, b);
+  document.getElementById("result").innerHTML = res;
 }
